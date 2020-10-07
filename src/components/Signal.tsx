6bd2ef9932cfcb572054fc63719b59be81e1f5e9
Ali Ahmed <ali.ahmed@punchcard.io>
2020-10-06 23:17:40 -0600

    add a four signal state machine

--- src/components/Signal.tsx
+++ src/components/Signal.tsx
@@ -1,79 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import styled from 'styled-components';
 import { rgba } from 'polished';
-import { Machine, StateValue } from 'xstate';
-import { useMachine } from '@xstate/react';
-import { black, trafficColors } from '../utils/constants';
+import { StateValueMap } from 'xstate';
+import {
+	black,
+	signalBodyBorderRadius,
+	signalBodyWidth,
+	signalLightRadius,
+	trafficColors,
+} from '../utils/constants';
+import { SignalColor } from '../utils/types';
 
-interface IProps {}
+interface IProps {
+	name: string;
+	currentLight: StateValueMap;
+}
 
 const SignalContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+	justify-content: space-around;
 	align-items: center;
 	border: 1px solid black;
-	width: 120px;
-	height: 270px;
-	padding: 5px 0;
-	justify-content: space-between;
-	border-radius: 10px;
+	width: ${signalBodyWidth}px;
+	padding: 4px 2px;
+	border-radius: ${signalBodyBorderRadius}px;
 	background: ${rgba(black, 0.6)};
 `;
 
 interface IColorProps {
 	bgColor: SignalColor;
-	current: StateValue;
+	currentLight: StateValueMap;
+	name: string;
 }
 
 const Color = styled.div<IColorProps>`
-	width: 80px;
-	height: 80px;
+	width: ${signalLightRadius}px;
+	height: ${signalLightRadius}px;
 	border-radius: 100%;
 	background-color: ${(props: IColorProps) =>
-		props.bgColor === props.current
+		props.currentLight[props.name] === undefined
+			? props.bgColor === 'red' ? trafficColors[props.bgColor]: rgba(trafficColors[props.bgColor], 0.4)
+			: props.bgColor === props.currentLight[props.name]
 			? trafficColors[props.bgColor]
 			: rgba(trafficColors[props.bgColor], 0.4)};
 	border: 3px solid black;
-	border-top: 6px solid black;
+	border-top: 5px solid black;
+	margin-bottom: 1px;
 `;
 
-const signalMachine = Machine({
-	id: 'signal',
-	initial: 'green',
-	states: {
-		green: {
-			after: {
-				3000: 'yellow',
-			},
-		},
-		red: {
-			after: {
-				3000: 'green',
-			},
-		},
-		yellow: {
-			after: {
-				1000: 'red',
-			},
-		},
-	},
-});
-
-type SignalColor = 'green' | 'red' | 'yellow';
-
-const Signal: React.FC<IProps> = (props) => {
-	const [current, send, service] = useMachine(signalMachine);
-
-	useEffect(() => {
-		send('CHANGE');
-	}, [send]);
-
+const Signal: React.FC<IProps> = ({ name, currentLight }) => {
 	return (
 		<SignalContainer>
-			<Color bgColor="red" current={current.value} />
-			<Color bgColor="yellow" current={current.value} />
-			<Color bgColor="green" current={current.value} />
+			<Color bgColor="red" currentLight={currentLight} name={name} />
+			<Color bgColor="yellow" currentLight={currentLight} name={name} />
+			<Color bgColor="green" currentLight={currentLight} name={name} />
 		</SignalContainer>
 	);
 };
